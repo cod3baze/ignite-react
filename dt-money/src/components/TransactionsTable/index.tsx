967827045b79069f28e9652e041e3f58c7e532dc
elias alexandre <elliasallex@gmail.com>
2021-11-30 19:05:00 -0300

    datas format (dt-money)

--- dt-money/src/components/TransactionsTable/index.tsx
+++ dt-money/src/components/TransactionsTable/index.tsx
@@ -36,7 +36,11 @@ export function TransactionsTable() {
           {transactions.map((transaction) => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>$ {transaction.amount}</td>
+              <td className={transaction.type}>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
               <td>{transaction.category}</td>
               <td>{transaction.createdAt}</td>
             </tr>
