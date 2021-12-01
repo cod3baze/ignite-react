cf8f34684a9948c2b4fa10d437db29b360241f38
elias alexandre <elliasallex@gmail.com>
2021-12-01 14:50:15 -0300

    redirect to Stripe checkoutSession

--- ig-news/src/components/SubscribeButton/index.tsx
+++ ig-news/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -15,6 +17,19 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     }
 
     // criação da checkout session
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({
+        sessionId,
+      });
+    } catch (error) {
+      alert(error.message);
+    }
   }
 
   return (
