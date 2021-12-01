f0493e0b6d129477321500f6fdc37be3d1507699
elias alexandre <elliasallex@gmail.com>
2021-12-01 15:12:10 -0300

    stripe: avoind duplicated users (ignews)

--- ig-news/src/pages/api/subscribe.ts
+++ ig-news/src/pages/api/subscribe.ts
@@ -1,20 +1,57 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import { query as q } from "faunadb";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+type IUser = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 export default async function subscribe(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
   if (request.method === "POST") {
+    // get logged user
     const session = await getSession({ req: request });
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-    });
+    // get user on DB
+    const user = await fauna.query<IUser>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
 
+    let customerId = user.data.stripe_customer_id;
+
+    /**
+     * se o user não esta cadastrado no striper cria um novo user,
+     * e atualiza o ID-stripe desse user no DB
+     */
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+      });
+
+      // update user customer id on DB
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      );
+
+      customerId = stripeCustomer.id;
+    }
+
+    // cria uma **sessão** do usuário atual
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ["card"],
       billing_address_collection: "required",
       line_items: [
