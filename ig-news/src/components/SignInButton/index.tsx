bcca40ec842cc39e4da0ea21faa984cba16eb81f
elias alexandre <elliasallex@gmail.com>
2021-11-30 23:41:13 -0300

    SignInButton handle user login state (ig.news)

--- ig-news/src/components/SignInButton/index.tsx
+++ ig-news/src/components/SignInButton/index.tsx
@@ -1,5 +1,21 @@
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  return <button type="button">Sign in with github</button>;
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="#04b361" />
+      elias alexandre
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="#eba417" />
+      Sign in with github
+    </button>
+  );
 }
