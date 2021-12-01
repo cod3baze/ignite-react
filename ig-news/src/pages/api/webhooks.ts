98bf2703ab3116b32e5acec7de6e5b009c582ebf
elias alexandre <elliasallex@gmail.com>
2021-12-01 20:40:53 -0300

    stripe: saving data from evets (ignews)

--- ig-news/src/pages/api/webhooks.ts
+++ ig-news/src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 import { Readable } from "stream";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscriptions";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -43,7 +44,24 @@ const webhooks = async (request: NextApiRequest, response: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log("evento recebido: ", event);
+      try {
+        switch (type) {
+          case "checkout.session.completed":
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString()
+            );
+            break;
+
+          default:
+            throw new Error("Unhandled event.");
+        }
+      } catch (err) {
+        return response.json({ error: "Webhook has failed" });
+      }
     }
 
     response.json({ received: true });
---
+++ ig-news/src/pages/posts.tsx
@@ -0,0 +1,7 @@
+export default function Posts() {
+  return (
+    <div>
+      <h1>hello, eliasallex</h1>
+    </div>
+  );
+}
