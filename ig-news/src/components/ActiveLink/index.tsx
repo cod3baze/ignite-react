a028bfefc250e116a77d5a73f4742e4e57d3bba5
elias alexandre <elliasallex@gmail.com>
2021-12-03 16:35:15 -0300

    ActiveLink with cloneElement (ignews)

---
+++ ig-news/src/components/ActiveLink/index.tsx
@@ -0,0 +1,30 @@
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+import { cloneElement } from "react";
+
+interface ActiveLinkProps extends LinkProps {
+  children: React.ReactNode;
+  activeClassName: string;
+}
+
+export function ActiveLink({
+  children,
+  activeClassName,
+  ...rest
+}: ActiveLinkProps) {
+  const { asPath } = useRouter();
+
+  const className = asPath === rest.href ? activeClassName : "";
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        className,
+      })}
+    </Link>
+  );
+}
+
+/**
+ * cloneElement: clone um component e pode adicionar propriedades a esse *clone*
+ */
--- ig-news/src/components/Header/index.tsx
+++ ig-news/src/components/Header/index.tsx
@@ -1,24 +1,22 @@
-import { useRouter } from "next/router";
-import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
 
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
 export function Header() {
-  const { asPath } = useRouter();
-
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
-        <nav>
-          <Link href="/">
-            <a className={asPath === "/" ? styles.active : ""}>Home</a>
-          </Link>
 
-          <Link href="/posts" prefetch>
-            <a className={asPath === "/posts" ? styles.active : ""}>Posts</a>
-          </Link>
+        <nav>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
