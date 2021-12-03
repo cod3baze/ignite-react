9a549f7cf01edc174b37bdf04ce3f35a179796db
elias alexandre <elliasallex@gmail.com>
2021-12-03 15:54:40 -0300

    prismic: client config (ignews)

--- ig-news/src/pages/posts/index.tsx
+++ ig-news/src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Primisc from "@prismicio/client";
+import { getPrismicClient } from "../../services/prismic";
 
 import styles from "./styles.module.scss";
 
@@ -48,3 +51,21 @@ export default function Posts() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Primisc.predicates.at("document.type", "publication")],
+    {
+      fetch: ["publication.title", "publication.content"],
+      pageSize: 100,
+    }
+  );
+
+  console.log(JSON.stringify(response, null, 2));
+
+  return {
+    props: {},
+  };
+};
