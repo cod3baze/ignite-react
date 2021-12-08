0f46cd1797ae1d98b36265677b6972d39f07c0d9
elias alexandre <elliasallex@gmail.com>
2021-12-07 22:09:04 -0300

    pagination style (dash.Go)

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -18,6 +18,7 @@ import {
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Pagination } from "../../components/Pagination";
 
 export default function UserList() {
   return (
@@ -134,6 +135,8 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
