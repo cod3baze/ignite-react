bd75da0e4e73b53119fb2a5b2c3dc4f978a458be
elias alexandre <elliasallex@gmail.com>
2021-12-15 20:45:43 -0300

    SSR API context (Auth.control)

---
+++ auth-control/src/utils/withSSRAuth.ts
@@ -0,0 +1,27 @@
+// usada em páginas que só serão acessadas por usuários logados
+
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { parseCookies } from "nookies";
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+
+    if (!cookies["nextauth.token"]) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+
+    return await fn(ctx);
+  };
+}
