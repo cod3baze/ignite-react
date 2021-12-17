eb5ba3e725b7cbcb47a1a0c3333650aedabcdf53
elias alexandre <elliasallex@gmail.com>
2021-12-16 21:04:37 -0300

    app example (react-perf)

--- perf-react/src/pages/index.tsx
+++ perf-react/src/pages/index.tsx
@@ -1,7 +1,39 @@
+import { FormEvent, useState } from "react";
+import { SearchResults } from "../components/SearchResults";
+
 export default function Home() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState<
+    Array<{ id: number; price: number; title: string }>
+  >([]);
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+    setResults(data);
+  }
+
   return (
-    <div className={styles.container}>
-      <h1>eliasallex</h1>
+    <div>
+      <h1>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input
+          type="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
+
+      <SearchResults results={results} />
     </div>
   );
 }
